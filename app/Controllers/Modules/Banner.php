c1ace415f0638288a4d1b0c20b3c00893ecce79c
DESKTOP-J67BSCC\DELL <kietnguyen1147@gmail.com>
2020-12-16 09:20:51 +0700

    banner module

---
+++ app/Controllers/Modules/Banner.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers\Modules;
+
+use App\Services\Queries;
+
+class Banner
+{
+    public function dataModule($module)
+    {
+        $banner = $this->getData();
+        return (object) [
+            'module' => $module,
+            'banner' => $banner
+        ];
+    }
+
+    public function getData()
+    {
+        return Queries::getBanner();
+    }
+}
---
+++ resources/views/template-parts/page/module-banner.blade.php
@@ -0,0 +1,50 @@
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ $data->banner[0]->url }}" alt="Los Angeles">
+      <div class="carousel-caption">
+        <h3>{{ $data->banner[0]->title }}</h3>
+        <h5>We had such a great time in LA!</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{ $data->banner[1]->url }}" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>{{ $data->banner[1]->title }}</h3>
+        <h5>We had such a great time in LA!</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{ $data->banner[2]->url }}" alt="New York">
+      <div class="carousel-caption">
+        <h3>{{ $data->banner[2]->title }}</h3>
+        <h5>We had such a great time in LA!</h5>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+{{-- <div id="myBanner" class="fluid-container">
+    <img src="{{ $data->banner[0]->url }}">
+    <p>{{ $data->banner[0]->title }}</p>
+</div> --}}
